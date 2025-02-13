85eede958ed7df0d331b7f194e393809eece64eb
Grace-Racasa <racasa.grace1@gmail.com>
2025-02-13 12:00:24 +0800

    added Activity

---
+++ Lab1_act1_Arithmetics.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Prelim;
+
+import java.util.Scanner;
+
+public class Lab1_act1_Arithmetics {
+    public static void main(String[] args) {
+  
+        int x = 10;
+        int y = 4;
+        int z = 6;
+       
+       
+      System.out.println(" 10 " + " * " + " 4 " + " + " + " 6 " + " = " + (x * y + z));
+      System.out.println("( 10 " + " - " + " 4 )" + " % " + " 6 " + " = " + (x - y) % z);
+      System.out.println("( 10 " + " + " + " 4 )" + " + " + " 6 )" + " / " + " 3 " + " = " + (x = y + z) /3);
+      System.out.println(" 10 " + " * " + " 6 " + " - " + "( 6 " + " * " + " 4 )" + " = " + (x * z - (y * y)));
+    
+    
+    }
+}
