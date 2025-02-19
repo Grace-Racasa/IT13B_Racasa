c01de3d2ca584ac3f839b0b246bbb10e77840edc
Grace-Racasa <racasa.grace1@gmail.com>
2025-02-19 09:36:53 +0800

    Update Lab1_act1_Arithmetics.java

--- RACASA_IT13B_ACTIVITIES/src/Prelim/Lab1_act1_Arithmetics.java
+++ RACASA_IT13B_ACTIVITIES/src/Prelim/Lab1_act1_Arithmetics.java
@@ -1,25 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Prelim;
 
 import java.util.Scanner;
 
-public class Lab1_act1_Arithmetics {
-    public static void main(String[] args) {
-  
-        int x = 10;
-        int y = 4;
-        int z = 6;
+public class Lab_act1_Arithmetics {
+    public static void main(String[] args){
+        
+        int first_number,second_number,third_number;
+        int x = 3;
        
-       
-      System.out.println(" 10 " + " * " + " 4 " + " + " + " 6 " + " = " + (x * y + z));
-      System.out.println("( 10 " + " - " + " 4 )" + " % " + " 6 " + " = " + (x - y) % z);
-      System.out.println("( 10 " + " + " + " 4 )" + " + " + " 6 )" + " / " + " 3 " + " = " + (x = y + z) /3);
-      System.out.println(" 10 " + " * " + " 6 " + " - " + "( 6 " + " * " + " 4 )" + " = " + (x * z - (y * y)));
-    
-    
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        first_number = in.nextInt();
+        System.out.print("Enter second number: ");
+        second_number = in.nextInt();
+        System.out.print("Enter third number: ");
+        third_number = in.nextInt();
+        
+        System.out.println(first_number + " * " + second_number + " + " + third_number + " = " + (first_number * second_number + third_number));
+        System.out.println("(" + first_number + " - " + second_number + ")" + " % " + third_number + " = " + ((first_number - second_number) % third_number));
+        System.out.println("(" + first_number + " + " + second_number + " + " + third_number + ")" + " / " + x + " = " + ((first_number + second_number + third_number)/ x));
+        System.out.println(first_number + " * " + third_number + " - " + "(" + second_number + " * " + second_number + ")" +  " = " + (first_number * third_number - (second_number * second_number)));
     }
 }
