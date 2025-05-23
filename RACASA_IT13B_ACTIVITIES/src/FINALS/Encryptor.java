17b05bc2e9a1095acbd2ad82defe40e2d98ab20b
Grace-Racasa <racasa.grace1@gmail.com>
2025-05-23 19:13:33 +0800

    added new

---
+++ RACASA_IT13B_ACTIVITIES/src/FINALS/Encryptor.java
@@ -0,0 +1,21 @@
+
+package FINALS;
+
+
+public class Encryptor {
+ public static String encrypt(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            result.append((char)(c + shift));
+        }
+        return result.toString();
+    }
+
+    public static String decrypt(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            result.append((char)(c - shift));
+        }
+        return result.toString();
+    }
+}
